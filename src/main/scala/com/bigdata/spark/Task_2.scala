013932b8ecbbfe11621b8bbbd070030ef00892a9
Ευστράτιος Σκαπέρδας <32629737+sskaperdas@users.noreply.github.com>
2022-01-12 18:16:54 +0200

    Add files via upload

--- src/main/scala/com/bigdata/spark/Task_2.scala
+++ src/main/scala/com/bigdata/spark/Task_2.scala
@@ -1,11 +1,10 @@
 //package com.bigdata.spark
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.ml.feature.{CountVectorizer, IDF, RegexTokenizer, VectorAssembler}
-import org.apache.spark.ml.linalg.Vector
-import org.apache.spark.mllib.linalg.distributed.{MatrixEntry, RowMatrix}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.{CountVectorizer, IDF, MinHashLSH, RegexTokenizer}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{LongType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 
 object Task_2 {
@@ -19,16 +18,11 @@ object Task_2 {
     val ss = SparkSession.builder().master("local[2]").appName("task1").getOrCreate()
     ss.sparkContext.setLogLevel("ERROR")
 
-    import ss.implicits._
-
     val inputFile = "./sample_preprocessed.csv"
     val rawDF = ss.read.option("header", "true").csv(inputFile)
     val tempDF = rawDF.select("member_name", "speech_processed").na.drop(Seq("member_name"))
 
     val df = tempDF.groupBy("member_name").agg(concat_ws(" ", collect_list("speech_processed")) as "speeches")
-
-    df.printSchema()
-    df.show(20)
 
     val tokenizer = new RegexTokenizer()
       .setPattern(" ")
@@ -36,9 +30,6 @@ object Task_2 {
       .setOutputCol("tokens")
 
     val tokenized_df = tokenizer.transform(df)
-
-    tokenized_df.printSchema()
-    tokenized_df.show(10)
 
     val vectorizer = new CountVectorizer()
       .setInputCol("tokens")
@@ -49,9 +40,6 @@ object Task_2 {
 
     val vectorizedDF = vectorizer.transform(tokenized_df).drop("speeches", "tokens")
 
-    vectorizedDF.printSchema()
-    vectorizedDF.show(25, truncate = false)
-
     val idf = new IDF()
       .setInputCol("features")
       .setOutputCol("features_idf")
@@ -59,46 +47,37 @@ object Task_2 {
 
     val inverseDF = idf.transform(vectorizedDF)
 
-    inverseDF.printSchema()
-    inverseDF.show(10, false)
-
-
-    val asDense = udf((v: Vector) => v.toDense) //transform to dense matrix
-
-    val vectorsDF = inverseDF.select('features_idf)
-      .withColumn("dense_features", asDense($"features_idf")).drop(col("features_idf"))
-      .withColumn("id", monotonically_increasing_id())
-    vectorsDF.show(11, false)
-
-    vectorsDF.printSchema()
-
-
-    val cosSimilarity = udf { (x: Vector, y: Vector) =>
-      val v1 = x.toArray
-      val v2 = y.toArray
-      val l1 = scala.math.sqrt(v1.map(x => x*x).sum)
-      val l2 = scala.math.sqrt(v2.map(x => x*x).sum)
-      val scalar = v1.zip(v2).map(p => p._1*p._2).sum
-      scalar/(l1*l2)
+    def addColumnIndex(df: DataFrame) = {
+      ss.sqlContext.createDataFrame(
+        df.rdd.zipWithIndex.map {
+          case (row, index) => Row.fromSeq(row.toSeq :+ index)
+        },
+        // Create schema for index column
+        StructType(df.schema.fields :+ StructField("index", LongType, nullable = false)))
     }
 
+    val inverseDFIndex = addColumnIndex(inverseDF).drop("member_name", "features")
 
-    val rows = new VectorAssembler().setInputCols(vectorsDF.columns).setOutputCol("vs")
-      .transform(vectorsDF)
-      .select("vs")
-      .rdd
+    inverseDFIndex.show(false)
 
-    val rdd_rows = rows.map(_.getAs[org.apache.spark.ml.linalg.Vector](0))
-      .map(org.apache.spark.mllib.linalg.Vectors.fromML)
+    val mh = new MinHashLSH()
+      .setNumHashTables(500000)
+      .setInputCol("features_idf")
+      .setOutputCol("hashes")
 
-    rdd_rows.foreach(f => println(f.toDense))
+    val model = mh.fit(inverseDFIndex)
 
-    val mat: RowMatrix = new RowMatrix(rdd_rows)
+    // Feature Transformation
+    println("The hashed dataset where hashed values are stored in the column 'hashes':")
+    model.transform(inverseDFIndex).show()
 
-    // Compute similar columns with estimation using DIMSUM
-    val approx = mat.columnSimilarities(.7)
-    val approxEntries = approx.entries.map { case MatrixEntry(i, j, v) => ((i, j), v) }
+    println("Approximately Jaccard distance smaller than 0.2:")
+    model.approxSimilarityJoin(inverseDFIndex, inverseDFIndex, .2, "JaccardDistance")
+      .select(col("datasetA.index").alias("idA"),
+        col("datasetB.index").alias("idB"),
+        col("JaccardDistance")).where(col("idA") =!= col("idB"))
+      .sort(col("JaccardDistance"))
+      .show()
 
-//    approxEntries.foreach(println)
   }
 }
